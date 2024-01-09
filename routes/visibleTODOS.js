e9ad8ce6b2b9f258395ecc10783f6c2242bfb563
evyatarsh6 <evyash6@gmail.com>
2024-01-09 13:41:04 +0200

    wip

--- routes/visibleTODOS.js
+++ routes/visibleTODOS.js
@@ -1,17 +1,21 @@
 const  { Router} = require('express')
 const { generateFilterKindQuery } = require('../queries/queries');
-const { getWantedDocumentsFromCollec } = require('../actions/getActions');
+// const { getWantedDocumentsFromCollec } = require('../actions/getActions');
+const getDB =  require('../getDB')
 
 const router = Router();
 
 router.get("/:filterKind", async (req, res) => {
   const filterKind = req.params.filterKind
+  const database =  await getDB()
+  const wantedCollection = await database.collection("TODOS")
   const query = generateFilterKindQuery(filterKind)
-  const results = await getWantedDocumentsFromCollec("TODOS", query)
-  // .toArray()
+  const results = await wantedCollection.findq(uery)
   res.send(results).status(200);
-
-
+  
+  
+  // const results = await getWantedDocumentsFromCollec("TODOS", query)
+  // .toArray()
   // let TODOSCollection = async () => await db.collection("TODOS");
   // let results = await TODOSCollection().find(query)
   //   .limit(50)
