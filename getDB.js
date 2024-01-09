dc37a3cec3ea0df4105a3daf8e0ad23ea3f36b18
evyatarsh6 <evyash6@gmail.com>
2024-01-09 11:39:54 +0200

    wip

--- getDB.js
+++ getDB.js
@@ -1,15 +1,14 @@
 const { MongoClient } = require('mongodb');
 
 // Replace 'your_database_name' with the actual name of your MongoDB database
-// const dbUrl = 'mongodb://localhost:27017/TODOLIST-Project-DB';
 
 const dbUrl = 'mongodb://127.0.0.1:27017/TODOLIST-Project-DB';
+// const dbUrl = 'mongodb://localhost:27017/TODOLIST-Project-DB';
+
 
 async function connectToMongo() {
-// async function connectToMongo(action, actionData) {
   const client = new MongoClient(dbUrl);
-    // , { useNewUrlParser: true, useUnifiedTopology: true }
-    // );
+  
   try {
     await client.connect();
     console.log('Connected to MongoDB');
